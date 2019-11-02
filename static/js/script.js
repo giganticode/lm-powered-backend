2aebf2ace3f430c9321daa9af01cb11b83185fc8
Moritz <moritz.griesser@gmail.com>
2019-11-02 22:33:34 +0100

    update webservice

--- static/js/script.js
+++ static/js/script.js
@@ -26,7 +26,7 @@ function insertLines(params) {
  // define a sequence of colors to color the single tokens
  const colors = ['#BC2D0E', '#0E4CC8', '#AB0BAD', '#0C6407', '#AAA111', '#3F11AA', '#D9980D', '#402D03', '#37158C'];
 
- function formatEntropies(data, target) {
+ function formatEntropies(data, target, colorMapping) {
      target.empty();
      let currentColorIndex = 0;
 
@@ -53,11 +53,13 @@ function insertLines(params) {
                      let newSpan = $(`<span class="token">${text}</span>`);
                      newSpan.css('color', color);
                      htmlLine.append(newSpan);
-                     UIkit.tooltip(newSpan, {title: `'${text}' -> entropy: ${entropy.toFixed(3)}`});
+                     let backgroundColor = mapEntropyToColor(entropy, colorMapping);
+                     newSpan.css('background-color', backgroundColor);
+                     UIkit.tooltip(newSpan, {title: `'${text}' -> entropy: ${(entropy || 0).toFixed(3)}`});
                      found = true;
                  }
                  else {
-                     htmlLine.append($(`<span class="token"> </span>`));
+                     htmlLine.append($(`<span class="token whitespace"> </span>`));
                      startIndex++;
                      endIndex++;
                  }
@@ -71,3 +73,175 @@ function insertLines(params) {
          target.append(htmlLine);
      }
  }
+
+ function mapEntropyToColor(entropy, colors) {
+    for (let i = 0; i < colors.length; i++) {
+        let min = colors[i].min;
+        let max = colors[i].max;
+        let color = colors[i].color;
+        if (entropy > min && entropy <= max) {
+            return color;
+        }
+    }
+    return "transparent";
+ }
+
+ function displayMetadata(metadata, target) {
+    target.empty();
+    target.append($(`<h3>Server-side-process information</h3>`))
+    target.append($(`<span>Time needed to load the model: <b>${metadata.time_model_loading}ms</b></span>`))
+    target.append($(`</br>`))
+    target.append($(`<span>Time needed to calculate the entropies: <b>${metadata.time_entropy_calculation}ms</b></span>`))
+    target.append($(`</br>`))
+    target.append($(`<span>Total server-side processing: <b>${metadata.total}ms</b></span>`))
+    target.append($(`</br>`))
+ }
+
+ $(document).ready(function() {
+    let modalSelectModel = UIkit.modal($('#modal-select-model'), {bgClose: false, escClose: false});
+    let modalColorMapping = UIkit.modal($('#modal-color-mapping'), {bgClose: false, escClose: false});
+
+    if (localStorage.getItem('colorMapping')) {
+        try {
+            colorMapping = JSON.parse(localStorage.getItem('colorMapping'));
+        } catch(e) {
+            // alert(e); // error in the above string (in this case, yes)!
+            // use default color mapping
+        }
+    }
+
+    /* color mapping */
+    for (let i = 0; i < colorMapping.length; i++) {
+        let color = colorMapping[i].color;
+        let min = colorMapping[i].min;
+        let max = colorMapping[i].max;
+        
+        let htmlItem = $(`
+        <div>
+            <label>
+                Min <input type="number" class="uk-input" value="${min}" id="min-${i}">
+            </label>
+            <label>
+                Max <input type="number" class="uk-input" value="${max}" id="max-${i}">
+            </label>
+            Color: <input id="color-${i}" type='text'>
+        </div>`);
+        $('#colors').append(htmlItem);
+
+        $(`#color-${i}`).spectrum({
+            color: color,
+            showAlpha: false,
+            clickoutFiresChange: false,
+            preferredFormat: "hex",
+            showInput: true,
+            change: function(color) {
+                // console.log("change", color)
+                // console.log(color.toHexString()); // #ff0000
+                colorMapping[i].color = color.toHexString();
+                // todo save colors in localStorage
+            }
+        })
+    }
+
+    $('#open-colors').click(function(e) {
+        e.preventDefault();
+        modalColorMapping.show();
+    });
+
+    $('#modal-color-mapping-save').click(function(e) {
+        let lastMax = 0;
+        let error = false;
+        // validate min/max
+        for (let i = 0; i < colorMapping.length; i++) {
+            let min = parseFloat($(`#min-${i}`).val());
+            let max = parseFloat($(`#max-${i}`).val());
+            colorMapping[i].min = min;
+            colorMapping[i].max = max;
+
+            console.log("Iteration: ", i, min, max)
+
+            if (min > max) {
+                error = "Incorrect values";
+                console.log("min > max", i, min, max)
+                $(`#min-${i}`).focus();
+                break;
+            }
+
+            if (min != lastMax) {
+                error = `Incorrect values, the value should be '${lastMax}'`;
+                console.log("min != lastMax", i, min, lastMax)
+                $(`#min-${i}`).focus();
+                break;
+            }
+
+            lastMax = max;
+        }
+
+        if (error !== false) {
+            UIkit.notification({message: error, status: 'danger', pos: 'top-right'})
+        } else {
+            modalColorMapping.hide();
+            let jsonEncoded = JSON.stringify(colorMapping);
+            localStorage.setItem('colorMapping', jsonEncoded);
+            UIkit.notification({message: "Configuration was saved successfully. Please recalculate the entropies to see the changes.", status: 'success', pos: 'top-right'})
+        }
+
+    });
+
+    $('#modal-color-mapping-abort').click(function(e) {
+        modalColorMapping.hide();
+        colorMapping = localStorage.getItem('colorMapping') || defaultColorMapping;
+    });
+    /* end color mapping */
+
+
+
+    if (!availableModels.includes(currentModel)) {
+        currentModel = availableModels[0];
+        currentModelName = availableModelNames[0];
+        localStorage.setItem("selectedModel", currentModel);
+        localStorage.setItem("selectedModelName", currentModelName);
+    }
+
+    function saveCurrentModel(selectedModel, selectedModelName) {
+        localStorage.setItem("selectedModel", selectedModel);
+        localStorage.setItem("selectedModelName", selectedModelName);
+        currentModel = selectedModel;
+        currentModelName = selectedModelName;
+    }
+
+    displayCurrentModel();
+
+    function displayCurrentModel() {
+        $('#current-model').text(currentModelName);
+    }
+
+    $('#open-selectlm-modal').click(function(e) {
+        $('#lm-select').val(currentModel);
+        if (!$('#lm-select').val()) {
+            $("#lm-select").val($("#lm-select option:first").val());
+        }
+        $('#lm-select').change();
+        modalSelectModel.show();
+    });
+
+    $('#modal-save').click(function(e) {
+        let selectedModel = $('#lm-select').val();
+        let selectedModelName = $("#lm-select option:selected").text();
+        saveCurrentModel(selectedModel, selectedModelName);
+        modalSelectModel.hide();
+        displayCurrentModel();
+    });
+
+    $('#modal-abort').click(function(e) {
+        modalSelectModel.hide();
+    });
+
+    $('#lm-select').change(function(e) {
+        let selectedLM = $(this).val();
+        $(`[data-identifier]`).hide();
+        $(`[data-identifier="${selectedLM}"]`).show();
+    });
+
+
+ });
--- webservice.py
+++ webservice.py
@@ -1,16 +1,21 @@
 import sys
 import os
+import math
 import hashlib 
 import mimetypes
 import time
+import pickle
 from datetime import datetime
 from decimal import Decimal
+
+from util.stopwatch import StopWatch
+
 # RegEx
 import re
 # Flask webservice
-from flask import Flask, render_template, request, json, redirect
+from flask import Flask, render_template, request, url_for, json, redirect, jsonify
+from flask_cors import CORS
 from json import JSONEncoder
-import pickle
 from decimal import Decimal
 from base64 import b64encode, b64decode
 
@@ -21,23 +26,66 @@ if langModelPath is None or not os.path.isdir(langModelPath):
     exit()
 
 sys.path.insert(0, langModelPath)
-# def get_entropy_for_each_line(trained_model: TrainedModel, file: str, verbose: bool = False, only_aggregated_entropies: bool = True)
+import langmodels.modelregistry as modelRegistry
 from langmodels.inference.entropies import get_entropy_for_each_line
-from langmodels.model import TrainedModel
 from langmodels.evaluation.common import EvaluationResult
+from langmodels.evaluation.evaluation import evaluate_model_on_string, evaluate_model_on_file, evaluate_model_on_path
 from dataprep.parse.model.metadata import PreprocessingMetadata
+from langmodels.model import TrainedModel, ModelDescription
+
+from util.modelinstance import ModelInstance
 
 PORT = 8080
 global rootPath
-global model
 global completionModel
 global root
 global files
 
+MODEL_ZOO_PATH_ENV_VAR = 'MODEL_ZOO_PATH'
+if MODEL_ZOO_PATH_ENV_VAR in os.environ:
+    MODEL_ZOO_PATH = os.environ[MODEL_ZOO_PATH_ENV_VAR]
+else:
+    MODEL_ZOO_PATH = os.path.join(os.environ['HOME'], 'modelzoo')
+
 rootPath = os.path.dirname(os.path.realpath(__file__))
+
+
+
+models = {}
+registeredModels = modelRegistry.query_all_models(cached = True)
+
+for model_description in registeredModels:
+    models[model_description.id] = ModelInstance(model_description)
+
+default_color_mapping = [
+    {'min': 0, 'max': 2, 'color': "green"},
+    {'min': 2, 'max': 4, 'color': "rgb(126, 128, 0)"},
+    {'min': 4, 'max': 8, 'color': "orange"},
+    {'min': 8, 'max': 16, 'color': "rgb(255, 72, 0)"},
+    {'min': 16, 'max': 25, 'color': "red"},
+]
 
 app = Flask(__name__)
 app.debug = True
+
+cors = CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+
+def print_json(value):
+    return json.dumps(value, sort_keys=True, indent=2)
+
+app.jinja_env.filters['print_json'] = print_json
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path, endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
 
 @app.route('/')
 def default():
@@ -51,27 +99,259 @@ def home_page():
         {'url': "/search", 'name': "Contextual search", 'description': "Perform contextual search queries", 'icon': "search"},
         {'url': "/overview", 'name': "Project overview", 'description': "Show all cached projects and files", 'icon': "database"},
     ]
-    return render_template("home.html", list = list, lmpath = langModelPath, modelzoopath = os.environ.get('MODEL_ZOO_PATH'))
+    return render_template("home.html", list = list, lmpath = langModelPath, modelzoopath = os.environ.get('MODEL_ZOO_PATH'), models = models, default_color_mapping = default_color_mapping)  
+
+def serialize_model(model:ModelInstance):
+    return {
+        'id': model.model_description.id,
+        'bpe_merges': model.model_description.bpe_merges,
+        'layers_config': model.model_description.layers_config,
+        'arch': model.model_description.arch,
+        'bin_entropy': model.model_description.bin_entropy,
+        'training_time_minutes_per_epoch': model.model_description.training_time_minutes_per_epoch,
+        'n_epochs': model.model_description.n_epochs,
+        'best_epoch': model.model_description.best_epoch,
+        'tags': model.model_description.tags,
+    }
+
+    #     id: str
+    # bpe_merges: str
+    # layers_config: str
+    # arch: str
+    # bin_entropy: float
+    # training_time_minutes_per_epoch: int
+    # n_epochs: int
+    # best_epoch: int
+    # tags: List[str]
+
+# API-routes
+@app.route('/api/models')
+def api_models():
+    serialized_models = [serialize_model(models[key]) for key in models]
+    response = jsonify({
+                'models': serialized_models,
+            })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/api/autocompletion', methods = ['POST'])
+def api_autocompletion():
+    sw = StopWatch()
+    sw.start()
+    stop_watch = StopWatch()
+    stop_watch.start()
+
+    data = json.loads(request.data)
+    content = data.get('content', '')
+    proposalsCount = int(data.get('proposalsCount', 10))
+    selected_model_name = data.get('model', list(models.keys())[0])
+    selected_model = models[selected_model_name]
+
+    model = selected_model.get_model()
+    sw_model_loading = stop_watch.elapsed()
+
+    stop_watch.start()
+    model.feed_text(content)
+    sw_feed = stop_watch.elapsed()
+
+    # this does not change the state of the completionModel:
+    stop_watch.start()
+    predictions = model.predict_next_full_token(n_suggestions = proposalsCount)
+    model.reset()
+    sw_predicions = stop_watch.elapsed()
+
+    response = jsonify({
+                'predictions': predictions,
+                'metadata': {
+                    'time_model_loading': sw_model_loading * 1000,
+                    'time_feed': sw_feed * 1000,   
+                    'time_predictions': sw_predicions * 1000,
+                    'total': sw.elapsed() * 1000
+                },
+            })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/api/languagemodel', methods = ['POST'])
+def api_languagemodel():
+    return user()
+
+@app.route('/api/project', methods = ['GET'])
+def api_project():
+    projects = [{'name': files[hash]['name'], 'path': files[hash]['uri']['path'], 'url': hash} for hash in files.keys()]
+
+    response = jsonify(projects)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+    
+@app.route('/api/project/<projecthash>', methods = ['GET'])
+def api_project_files(projecthash):
+    project = files[projecthash]
+    
+    # project_files = [{'name': project['files'][hash]['name'], 'path': project['files'][hash]['path'], 'relpath': project['files'][hash]['relpath'], 'hash': hash} for hash in project['files'].keys()]
+    response = jsonify(project)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response    
+
+@app.route('/api/project/<projecthash>/<filehash>', methods = ['GET'])
+def api_project_files_detail(projecthash, filehash):
+    project = files[projecthash]
+    file = project['files'][filehash]
+    # file_content_path = os.path.join(rootPath, 'cache', 'input', projecthash, filehash)
+    file_entropy_path = os.path.join(rootPath, 'cache', 'output', projecthash, filehash)
+    entropies = []
+
+    if os.path.isfile(file_entropy_path):
+        with open(file_entropy_path, 'r') as f:
+            entropies = json.load(f)
+            # file_ontent = content_file.read().splitlines()
+            f.close()
+
+    # project_files = project['files']
+    response = jsonify({
+        'file': file,
+        'entropies': entropies
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response       
+
+@app.route('/api/compare', methods = ['POST'])
+def api_compare():
+    stop_watch = StopWatch()
+    sw = StopWatch()
+    sw.start()
+    print("GOT COMPARE REQUEST ========================================================")
+    data = json.loads(request.data)
+    print(data)
+
+    extension = data.get('extension', '')
+    languageId = data.get('languageId', '')
+    timestamp = float(data.get('timestamp', 1))/1000
+    content = data.get('content', '')
+    resetContext = data.get('resetContext', False)
+    selected_model_name1 = data.get('model1', list(models.keys())[0])
+    selected_model_name2 = data.get('model2', list(models.keys())[0])
+
+    selected_model1 = models[selected_model_name1]
+    selected_model2 = models[selected_model_name2]
+
+    stop_watch.start()
+    model1 = selected_model1.get_model()
+    sw_model1_loading = stop_watch.elapsed()
+
+    stop_watch.start()
+    model2 = selected_model2.get_model()
+    sw_model2_loading = stop_watch.elapsed()
+
+    if not languageId in ['java', 'JAVA']:
+        print("language " + languageId + " not supported")
+        return {'error': 'language not supported yet'}, 404
+
+    stop_watch.start()
+    entropies1 = calculatelEntropiesOfString(model1, content, languageId) 
+    entropies1 = [custom_to_dict(custom) for custom in entropies1]
+    if (resetContext):
+        model1.reset()
+    sw_entropy1_calculation = stop_watch.elapsed()
+
+    stop_watch.start()
+    entropies2 = calculatelEntropiesOfString(model2, content, languageId) 
+    entropies2 = [custom_to_dict(custom) for custom in entropies2]
+    if (resetContext):
+        model2.reset()
+    sw_entropy2_calculation = stop_watch.elapsed()
+
+    response = jsonify({
+        'entropies1': entropies1,
+        'entropies2': entropies2,
+        'metadata': {
+            'time_model1_loading': sw_model1_loading * 1000,
+            'time_model2_loading': sw_model2_loading * 1000,
+            'time_entropy1_calculation': sw_entropy1_calculation * 1000,   
+            'time_entropy2_calculation': sw_entropy2_calculation * 1000,   
+            'total': sw.elapsed() * 1000
+        },
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/api/search', methods = ['POST'])
+def api_search():
+    sw = StopWatch()
+    sw.start()
+    stop_watch = StopWatch()
+    stop_watch.start()
+
+    data = json.loads(request.data)
+    content = data.get('content', '')
+    search = data.get('search', '')
+    searchInterval = int(data.get('searchInterval', 10))
+    selected_model_name = data.get('model', list(models.keys())[0])
+    selected_model = models[selected_model_name]
+
+    model = selected_model.get_model()
+    sw_model_loading = stop_watch.elapsed()
+
+    lines = content.splitlines()
+
+    originalContent = lines.copy()
+    searchContent = lines.copy()
+    i = 0
+    while i < len(originalContent):
+        originalContent.insert(i, '//')
+        searchContent.insert(i, '//' + search)
+        i += (searchInterval + 1)
+
+    stop_watch.start()
+    originalEntropies = calculatelEntropiesOfString(model, "\n".join(originalContent), 'java')
+    model.reset()
+    sw_original_entropies = stop_watch.elapsed()
+
+    stop_watch.start()
+    searchEntropies = calculatelEntropiesOfString(model, "\n".join(searchContent), 'java')
+    model.reset()
+    sw_search_entropies = stop_watch.elapsed()
+
+
+    ret = {}
+    ret['originalContent'] = (originalContent)
+    ret['searchContent'] = (searchContent)
+    ret['originalEntropy'] = [custom_to_dict(custom) for custom in originalEntropies]
+    ret['searchEntropy'] = [custom_to_dict(custom) for custom in searchEntropies]
+
+    response = jsonify({
+        'metadata': {
+                        'time_model_loading': sw_model_loading * 1000,
+                        'time_original_entropies': sw_original_entropies * 1000,   
+                        'time_search_entropies': sw_search_entropies * 1000,
+                        'total': sw.elapsed() * 1000
+                    },
+        'entropies': ret
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+# end API-routes
 
 @app.route('/entropy')
 def entropy_page():
-    return render_template("entropy.html")      
+    return render_template("entropy.html", models = models, default_color_mapping = default_color_mapping)      
 
 @app.route('/completion')
 def completion_page():
-	return render_template("completion.html")    
+	return render_template("completion.html", models = models, default_color_mapping = default_color_mapping)       
 
 @app.route('/overview')
 def overview_page():
-    return render_template("projectoverview.html", files = files)         
+    return render_template("projectoverview.html", files = files, models = models, default_color_mapping = default_color_mapping)         
 
 @app.route('/search')
 def search_page():
-    return render_template("search.html")     
+    return render_template("search.html", models = models, default_color_mapping = default_color_mapping)  
 
 @app.route('/overview/<projecthash>')
 def overview_project(projecthash):
-    return render_template("fileoverview.html", projecthash = projecthash, project = files[projecthash])   
+    return render_template("fileoverview.html", projecthash = projecthash, project = files[projecthash], models = models, default_color_mapping = default_color_mapping)    
 
 @app.route('/overview/<projecthash>/<filehash>')
 def overview_file(projecthash, filehash):
@@ -94,7 +374,7 @@ def overview_file(projecthash, filehash):
     else:
         entropyContent = map(lambda x: "", fileContent)
     
-    return render_template("filedetail.html", file = file, fileContent = fileContent, entropyContentJson = json.dumps(entropyContent), entropyContent = entropyContent, projecthash = projecthash, filehash = filehash)   
+    return render_template("filedetail.html", file = file, fileContent = fileContent, entropyContentJson = json.dumps(entropyContent), entropyContent = entropyContent, projecthash = projecthash, filehash = filehash, models = models, default_color_mapping = default_color_mapping)   
 
 @app.route('/search', methods = ['POST'])
 def search():
@@ -102,6 +382,10 @@ def search():
     content = data.get('content', '')
     search = data.get('search', '')
     searchInterval = int(data.get('searchInterval', 10))
+    selected_model_name = data.get('model', list(models.keys())[0])
+    selected_model = models[selected_model_name]
+
+    model = selected_model.get_model()
 
     lines = content.splitlines()
 
@@ -113,8 +397,8 @@ def search():
         searchContent.insert(i, '//' + search)
         i += (searchInterval + 1)
 
-    originalEntropies = calculatelEntropiesOfString("\n".join(originalContent), 'original.java')
-    searchEntropies = calculatelEntropiesOfString("\n".join(searchContent), 'search.java')
+    originalEntropies = calculatelEntropiesOfString(model, "\n".join(originalContent), 'java')
+    searchEntropies = calculatelEntropiesOfString(model, "\n".join(searchContent), 'java')
 
     ret = {}
     ret['originalContent'] = (originalContent)
@@ -122,49 +406,39 @@ def search():
     ret['originalEntropy'] = [custom_to_dict(custom) for custom in originalEntropies]
     ret['searchEntropy'] = [custom_to_dict(custom) for custom in searchEntropies]
 
+    model.reset()
     encoded = json.dumps(ret)
     return encoded
 
 # Calculates and retun the entopies of a given inputfile
-def calculatelEntropies(inputPath):
-    only_aggregated_entropies = False
-    if only_aggregated_entropies :
-        entropies = get_entropy_for_each_line(model, inputPath, False, True)  
-        return entropies
-    else:
-        entropies = get_entropy_for_each_line(model, inputPath, False, False)  
-        print("Entropies Calculated....======================================") 
-        # print(entropies)
-
+def calculatelEntropiesOfString(model: TrainedModel, text: str, extension = 'java'):
+    entropies = evaluate_model_on_string(model, text, extension)
     return entropies
-
-# Calculates and retun the entopies of a given string - the string is saved to a file (name specified) in the temp folder
-def calculatelEntropiesOfString(string, name):
-    inputFile = os.path.join(rootPath, 'cache', 'temp', name)
-
-    with open(inputFile, 'w') as outfile:		
-        outfile.write(string)
-        outfile.close()	
-
-    return calculatelEntropies(inputFile)
 
 @app.route('/autocompletion', methods = ['POST'])
 def autocompletion():
     data = json.loads(request.data)
     content = data.get('content', '')
     proposalsCount = int(data.get('proposalsCount', 10))
+    selected_model_name = data.get('model', list(models.keys())[0])
+    selected_model = models[selected_model_name]
 
-    completionModel.feed_text(content)
+    model = selected_model.get_model()
+
+    model.feed_text(content)
 
     # this does not change the state of the completionModel:
-    predictions = completionModel.predict_next_full_token(n_suggestions = proposalsCount)
-    completionModel.reset()
+    predictions = model.predict_next_full_token(n_suggestions = proposalsCount)
+    model.reset()
 
     encoded = json.dumps(predictions)
+
     return encoded
 
 @app.route('/languagemodel', methods = ['POST'])
 def user():
+    sw = StopWatch()
+    sw.start()
     print("GOT LM REQUEST ========================================================")
     data = json.loads(request.data)
 
@@ -175,6 +449,16 @@ def user():
     timestamp = float(data.get('timestamp', 1))/1000
     content = data.get('content', '')
     workspaceFolder = data.get('workspaceFolder', None)
+    resetContext = data.get('resetContext', False)
+    selected_model_name = data.get('model', list(models.keys())[0])
+    selected_model = models[selected_model_name]
+
+    stop_watch = StopWatch()
+
+    stop_watch.start()
+    model = selected_model.get_model()
+    sw_model_loading = stop_watch.elapsed()
+    sw_entropy_calculation = 0
 
     if not languageId in ['java', 'JAVA']:
         print("language " + languageId + " not supported")
@@ -230,19 +514,37 @@ def user():
         f.write(content)
         f.close()
 
-        entropies = calculatelEntropies(savePath) 
+        stop_watch.start()
+        entropies = calculatelEntropiesOfString(model, content, languageId) 
+        sw_entropy_calculation = stop_watch.elapsed()
         print("entropies calculated")
+        print("ENTROPIES")
+        print(entropies)
         
         # write entropies to file (json-format)
         with open(path, 'w') as f:
             json.dump([custom_to_dict(custom) for custom in entropies], f)
             f.close()	
 
+    if (resetContext):
+        model.reset()
+
     if noReturn == True:
         print("NO return data...")
     else:
         with open(path, 'r') as content_file:
-            return content_file.read()
+            entropies = content_file.read()
+            entropies = json.loads(entropies)
+            response = jsonify({
+                'entropies': entropies,
+                'metadata': {
+                    'time_model_loading': sw_model_loading * 1000,
+                    'time_entropy_calculation': sw_entropy_calculation * 1000,   
+                    'total': sw.elapsed() * 1000
+                },
+            })
+            response.headers.add('Access-Control-Allow-Origin', '*')
+            return response
 
     return "request processed"
 
@@ -281,10 +583,8 @@ checkOrCreate(os.path.join(rootPath, 'cache', 'output'))
 
 if __name__ == '__main__':
     print("Starting WebServer on Port ", PORT)
-    print("Loading Language Model")
-    model = TrainedModel.get_default_model() # get_tiny_model()
     print("Loading Completion-Language Model")
-    completionModel = TrainedModel.get_default_model() # get_tiny_model()
+    #completionModel = load_model_by_name(SMALL_MODEL_NAME)
     print("Language Model loaded")
 
     app.run(use_reloader=True, port=PORT) #host='0.0.0.0'
